140a684c82a072ebaa22d9b5dea0a3919a09d2b3
repl.it user <replituser@example.com>
2021-03-12 20:13:35 +0000

    primer intenteo imprimir

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ def create_spend_chart (categories) :
   lista2 = []
   total = 0
   count = 0
+  result = 'Percentage spent by category'
   for element in categories :
     #print (element.ledger)
     result = element.cal_withdraw () #llamamos a una funcion ()
@@ -83,15 +84,85 @@ def create_spend_chart (categories) :
     while count < 1 :
       for i in lista1 :
         percentage = 100 * float(i) / total
-        tupla = (element.name, percentage)
-        print (tupla)
+        tupla = (percentage,element.name)
         lista2.append(tupla)
+        lista2.sort(reverse=True)
         count = count + 1
-  print (lista2) 
-  
+  print (lista2)
+
+  sum = 0
+  for v,k in lista2 :
+    if v <= 100 :
+      while sum < 1 :
+        a = '100|' + ' ' + 'o'
+        sum = sum + 1
+      a = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 90 :
+      while sum < 1 :
+        b = '90|' + ' ' + 'o'
+        sum = sum + 1
+      b = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 80 :
+      while sum < 1 :
+        b = '80|' + ' ' + 'o'
+        sum = sum + 1
+      b = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 70 :
+      while sum < 1 :
+        c = '70|' + ' ' + 'o'
+        sum = sum + 1
+      c = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 60 :
+      while sum < 1 :
+        d = '60|' + ' ' + 'o'
+        sum = sum + 1
+      d = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 50 :
+      while sum < 1 :
+        e = '50|' + ' ' + 'o'
+        sum = sum + 1
+      e = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 40 :
+      while sum < 1 :
+        f = '40|' + ' ' + 'o'
+        sum = sum + 1
+      f = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 30 :
+      while sum < 1 :
+        f = '30|' + ' ' + 'o'
+        sum = sum + 1
+      f = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 20 :
+      while sum < 1 :
+        g = '20|' + ' ' + 'o'
+        sum = sum + 1
+      g = ' ' + 'o'
+  sum = 0
+  for v,k in lista2 :
+    if v <= 10 :
+      while sum < 1 :
+        h = '10|' + ' ' + 'o'
+        sum = sum + 1
+      h = ' ' + 'o'
 
 
-
+  result = result + '\n' + a + '\n' + b + '\n'+ c + '\n' + d + '\n' + e + '\n' + f + '\n' + g + '\n' + h
 
 
 food = Category ('food')
